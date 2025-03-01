a1cde40c0c20969982dadc4b9a9c03a41c0bdaeb
Izaz-Bin-Touhid <izazbintouhid@gmail.com>
2025-03-02 04:28:15 +0600

    Sixth

--- script.js
+++ script.js
@@ -1,47 +1,80 @@
 const button = document.querySelectorAll(".btn");
-const counter2 = document.getElementById('middle');
-const counter1 = document.getElementById('top');
-const sideBar = document.getElementById('side');
-const text = document.querySelectorAll('text');
+const counter2 = document.getElementById("middle");
+const counter1 = document.getElementById("top");
+const sideBar = document.getElementById("side");
+const text = document.querySelectorAll("text");
+const sideBtn = document.getElementById("sideBtn");
+const tagTime = document.getElementsByClassName("tag-time");
 
-for( let i=0;i<button.length;i++){
-    const comBtn = button[i];
-    comBtn.addEventListener("click",function(event){
-        alert("Board Updated Successfully");
+let count = 0;
+for (let i = 0; i < button.length; i++) {
+  const comBtn = button[i];
+  comBtn.addEventListener("click", function (event) {
+    // Last Button
+    count++;
+    if (count === button.length) {
+      alert("congrats!!! You have completed all the current task");
+      event.target.classList.add("clicked");
+      event.target.disabled = true;
 
-        let value = parseInt(counter2.innerText);
-        value--;
-        counter2.innerText=value;
+      let value = parseInt(counter2.innerText);
+      value--;
+      counter2.innerText = value;
 
-        let value1 = parseInt(counter1.innerText);
-        value1++;
-        counter1.innerText=value1;
+      let value1 = parseInt(counter1.innerText);
+      value1++;
+      counter1.innerText = value1;
 
-        event.target.classList.add('clicked');
+      const now = new Date();
+      const timeString = now.toLocaleTimeString();
+      const customMessage = "You have completed the task at";
 
-        event.target.disabled = true;
+      const text1 = text.innerText;
 
-        
+      const timeElement = document.createElement("p");
+      timeElement.innerText =` ${customMessage} ${text1} ${timeString}`;
+      timeElement.classList.add("time-tag");
+      sideBar.appendChild(timeElement);
 
-        const now = new Date();
-        const timeString = now.toLocaleTimeString();
-        const customMessage = "You have completed the task at";
+      return;
+    }
 
-        const text1 = text.innerText;
-        
-        const timeElement = document.createElement('p');
-        timeElement.innerText = `${customMessage} ${text1} ${timeString}`;
-        timeElement.classList.add('time-tag');
-        sideBar.appendChild(timeElement);
+    alert("Board Updated Successfully");
 
-        
+    let value = parseInt(counter2.innerText);
+    value--;
+    counter2.innerText = value;
 
-    })
-    
+    let value1 = parseInt(counter1.innerText);
+    value1++;
+    counter1.innerText = value1;
+
+    event.target.classList.add("clicked");
+
+    event.target.disabled = true;
+
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    const customMessage = "You have completed the task at";
+
+    const text1 = text.innerText;
+
+    const timeElement = document.createElement("p");
+    timeElement.innerText =  `${customMessage} ${text1} ${timeString}`;
+    timeElement.classList.add("time-tag");
+    sideBar.appendChild(timeElement);
+  });
 }
-alert()
 
 
+
+sideBtn.addEventListener("click", function () {
+    const tagTimeElements = document.querySelectorAll(".time-tag");
+    tagTimeElements.forEach((element) => {
+      element.remove();
+    });
+  });
+  
 
 
 
